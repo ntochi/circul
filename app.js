07e169633be7020f9e34bb37e4185be3daf4a053
Nancy Tochi O <nancytochi24@gmail.com>
2021-09-09 20:28:56 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const port = process.env.PORT || process.env.IP || 3009;
+
+const app = express();
+
+// View engine setup
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("landing")
+});
+
+
+app.listen(port, () => {
+    console.log('Circul Has Started');
+});
