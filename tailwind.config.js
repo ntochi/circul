2a311f7f53663e77189116f13294374bf85b92d0
Nancy Tochi O <nancytochi24@gmail.com>
2021-09-19 15:20:57 -0400

    first draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,13 +18,26 @@ module.exports = {
           rhythm: '#5D5D81',
           violet: '#3B3355',
           light: '#FAF6FF'
+        },
+
+        yellow: {
+          mustard: '#FFDB58'
         }
-      }
+      },
+
+      textShadow: {
+        'purple': '8px 8px 8px rgba(102, 51, 153, 1)',
+        '3xl': '0 0 3px rgba(0, 0, 0, .8), 0 0 5px rgba(0, 0, 0, .9)',
+      },
     },
 
   },
+  
   variants: {
     extend: {},
   },
-  plugins: [],
+
+  plugins: [
+    require('tailwindcss-textshadow')
+  ],
 }
